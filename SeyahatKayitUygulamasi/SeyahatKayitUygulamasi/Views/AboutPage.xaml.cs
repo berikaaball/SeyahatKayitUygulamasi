375c662addb1b950eabd191bf8a41db1a84c93aa
Berika Bal <berikaaball@gmail.com>
2024-07-16 01:52:57 +0300

    Login page created

---
+++ SeyahatKayitUygulamasi/SeyahatKayitUygulamasi/Views/AboutPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SeyahatKayitUygulamasi.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            bool IsEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool IsPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if (IsEmailEmpty || IsPasswordEmpty)
+            {
+                await DisplayAlert("Uyarı", "Girdiğiniz değerleri kontrol ediniz.", "Tamam");
+            }
+            else 
+            { 
+                Navigation.PushAsync(new HomePage());
+            }
+
+
+        }
+    }
+}
---
+++ SeyahatKayitUygulamasi/SeyahatKayitUygulamasi/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SeyahatKayitUygulamasi.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SeyahatKayitUygulamasi.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
